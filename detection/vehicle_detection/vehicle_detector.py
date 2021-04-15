b5156d5d8b145acad982b0a30b0ba9abad922968
PommesPeter <434596665@qq.com>
2021-04-16 01:43:40 +0800

    fixed bug

--- detection/vehicle_detection/vehicle_detector.py
+++ detection/vehicle_detection/vehicle_detector.py
@@ -96,21 +96,6 @@ class VehicleDetector:
         self.load_camera_parameters("distance")
         self.logger.log_customize("Camera distance parameters ready...", icon="success", color="green")
 
-        self.logger.log_customize("Connect with wireless sensor...", icon="success", color="green")
-        self.build_serial_messager()
-        self.logger.log_customize("Connected...", icon="success", color="green")
-
-    def build_serial_messager(self):
-        meter_config = ParseConfig("../config/meter.json", "message")
-        self.meter_cfg = meter_config.read_config()
-        try:
-            self.cmd = Serial(
-                self.meter_cfg["port"], self.meter_cfg["baudrate"],
-                parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE,
-                timeout=self.meter_cfg["timeout"])
-        except:
-            raise ValueError("No such Serial Port to open...")
-
     def build_torch_detector(self):
         self.model_path = self.cfg["model"]
         self.logger.log_customize("Loading model...", icon="success", color="green")
@@ -175,10 +160,6 @@ class VehicleDetector:
                 class_id = int(detection.id)
                 color = self.palette[class_id]
                 det_label = labels[class_id - 1] if labels and len(labels) >= class_id else '#{}'.format(class_id)
-                if distance < self.cfg["dis_threshold"]:
-                    self.cmd.write("1ab".encode("utf-8"))
-                else:
-                    self.cmd.write("0ab".encode("utf-8"))
                 print('{:^9} | {:10f} | {:4} | {:4} | {:4} | {:4} | {:2} '
                       .format(det_label, detection.score, xmin, ymin, xmax, ymax, round(distance[0], 2)))
                 cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), color, 2)
